1db3104af0c2509f708962baa1ede421de6b6d94
andrea <andrea@andrea-K52F>
2015-04-24 13:30:25 +0200

    Apportate modifiche al gameactivity. Create funzioni di recupero della posizione (latitudine, longitudine), e messo in cantiere il metodo updateGUI, che servirà a muovere il pallino blu sulla mappa. Modificata anche la risorsa del pallino blu.

--- pro_z/src/com/example/pro_z/GameActivity.java
+++ pro_z/src/com/example/pro_z/GameActivity.java
@@ -1,12 +1,44 @@
 package com.example.pro_z;
 
 import android.app.Activity;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
 public class GameActivity extends Activity {
+
+	private LocationManager manager;
+	private static final int MIN_TIME = 10000;
+	private static final int MIN_DISTANCE = 20;
+	String locationProvider = LocationManager.GPS_PROVIDER;
+	private double latitude;
+	private double longitude;
+
+	LocationListener listener = new LocationListener() {
+
+		@Override
+		public void onStatusChanged(String provider, int status, Bundle extras) {
+		}
+
+		@Override
+		public void onProviderEnabled(String provider) {
+		}
+
+		@Override
+		public void onProviderDisabled(String provider) {
+		}
+
+		@Override
+		public void onLocationChanged(Location location) {
+			latitude = location.getLatitude();
+			longitude = location.getLongitude();
+		}
+
+	};
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -16,11 +48,37 @@ public class GameActivity extends Activity {
 	}
 
 	@Override
+	protected void onResume() {
+		super.onResume();
+		manager = (LocationManager) getSystemService(LOCATION_SERVICE);
+		manager.requestLocationUpdates(locationProvider, MIN_TIME,
+				MIN_DISTANCE, listener);
+		Location location = manager
+				.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+		if (location != null)
+			updateGUI(location);
+
+	}
+
+	public void updateGUI(Location location) {
+		// TODO metodo che controlla se è dentro al quadrato o no, se è vero
+		// calcola la posizione con dx e dy rispetto ad un punto e posiziona il
+		// pallino sulla mappa di conseguenza
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		if (manager != null && manager.isProviderEnabled(locationProvider))
+			manager.removeUpdates(listener);
+	}
+
+	@Override
 	public void onBackPressed() {
 		Toast.makeText(this, "Adios!", Toast.LENGTH_SHORT).show();
 		super.onBackPressed();
 	}
-	
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		getMenuInflater().inflate(R.menu.game, menu);
--- pro_z/src/com/example/pro_z/MainActivity.java
+++ pro_z/src/com/example/pro_z/MainActivity.java
@@ -1,16 +1,22 @@
 package com.example.pro_z;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		//TODO grafica per iniziare la partita (posizionare bottone ecc...)
 	}
 
 	@Override
